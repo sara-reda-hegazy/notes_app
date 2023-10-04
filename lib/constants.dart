9618d31ee1a445049d363905deef23183a167f25
sara-reda-hegazy <sr717160@gmail.com>
2023-10-04 18:26:20 +0300

    init hive and open note box

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/animation.dart';
 
 const kprimaryColor =  Color(0xff62FcD7);
+const kNotesBox ='notes_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notesapp/constants.dart';
 import 'package:notesapp/views/notes_view.dart';
 
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const MyApp());
 }
 
