410a39a9fc15eaf1f076aded6390e4b104bafbb9
sara-reda-hegazy <sr717160@gmail.com>
2023-10-08 13:51:12 +0300

    create fetch notes method

--- lib/cubits/add_note_cubit/notes_cubit/notes_state.dart
+++ lib/cubits/add_note_cubit/notes_cubit/notes_state.dart
@@ -4,7 +4,7 @@ part of 'notes_cubit.dart';
 sealed class NotesState {}
 
 final class NotesInitial extends NotesState {}
-final class NotesLoadind extends NotesState {}
+final class NotesLoading extends NotesState {}
 final class NotesSuccess extends NotesState {
   final List<NoteModel>notes;
 
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notesapp/cubits/add_note_cubit/notes_cubit/notes_cubit.dart';
 import 'package:notesapp/widgets/add_note_bottom_sheet.dart';
 import 'package:notesapp/widgets/notes_view_body.dart';
 
@@ -7,20 +9,25 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-      
-      body:const NotesViewBody(),
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        showModalBottomSheet(context: context, builder:(context) {
-          return const AddNoteBottomSheet();
-        },
-        isScrollControlled: true,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16),
-        )
-        );
-      },
-      child:const Icon(Icons.add),),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        body: const NotesViewBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return const AddNoteBottomSheet();
+                },
+                isScrollControlled: true,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ));
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
     );
   }
 }
